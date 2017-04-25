f49697094fb4222cb890e868c2b3b132f1b9067e
Scott Heng <scottheng3@gmail.com>
2017-04-25 02:24:41 -0700

    add searchable feature

--- lib/searchable.rb
+++ lib/searchable.rb
@@ -3,10 +3,19 @@ require_relative 'sql_object'
 
 module Searchable
   def where(params)
-    
+    where_line = params.keys.map { |key| "#{key} = ?" }.join(" AND ")
+    results = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+    parse_all(results)
   end
 end
 
 class SQLObject
-
+  extend Searchable
 end
