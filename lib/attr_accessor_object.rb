7ce83316487b02faaffd147e7d823c90790a8c0a
Scott Heng <scottheng3@gmail.com>
2017-04-25 01:38:32 -0700

    define setter/getter instance methods

--- lib/attr_accessor_object.rb
+++ lib/attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
+    names.each do |name|
+      instance_var_name = "@#{name}".to_sym
+      define_method(name) do 
+        instance_variable_get(instance_var_name)
+      end
+      
+      define_method("#{name}=") do |value|
+        instance_variable_set(instance_var_name, value)
+      end
+    end
 
   end
 end
