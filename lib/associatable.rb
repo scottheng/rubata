402c5274a10611a188ed456c9fa8620cefdadc83
Scott Heng <scottheng3@gmail.com>
2017-04-26 22:32:04 -0700

    add belongsto and hasmany options

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -9,30 +9,50 @@ class AssocOptions
   )
 
   def model_class
-
+    @class_name.constantize
   end
 
   def table_name
-
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      class_name: name.to_s.camelcase,
+      primary_key: :id
+    }
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-
+    defaults = {
+      :foreign_key => "#{self_class_name.underscore}_id".to_sym,
+      :class_name => name.to_s.singularize.camelcase,
+      :primary_key => :id
+    }
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 module Associatable
 
   def belongs_to(name, options = {})
-
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
+    end
+    
   end
 
   def has_many(name, options = {})
@@ -45,5 +65,5 @@ module Associatable
 end
 
 class SQLObject
-
+ 
 end
